60feb128119ed51b785a8fbe88bf8b03f7defbdd
Gabriele <gabriele.belluardo@outlook.it>
2020-08-14 16:15:01 +0200

    - fix cache token path - fix google scraper - fix  main loop

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use rspotify::client::Spotify;
 use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
 use rspotify::util::get_token;
 use scraper::{Html, Selector};
+use tokio::fs::create_dir_all;
 use tokio::process::Command;
 use tokio::time::delay_for;
 
 use std::env;
 use std::fmt;
+use std::path::PathBuf;
 use std::time::Duration;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -80,23 +82,25 @@ async fn main() -> Result<()> {
     dotenv().ok();
 
     let spotify = init_spotify().await?;
-    let mut track = Track::new();
+    let mut current_track = Track::new();
 
     loop {
-        let current_track = get_track(&spotify).await?;
+        let track = get_track(&spotify).await?;
         if current_track != track {
-            track = current_track;
-
             Command::new("clear").spawn()?.await?;
-
-            let t = track.clone().lyrics(&get_track_lyrics(&track).await?);
-            println!("{}", t);
+            current_track = match get_track_lyrics(&track).await? {
+                Some(lyrics) => track.lyrics(&lyrics),
+                None => track,
+            };
+            println!("{}", current_track);
         }
         delay_for(Duration::from_millis(300)).await
     }
 }
 
-async fn get_track_lyrics(t: &Track) -> Result<String> {
+async fn get_track_lyrics(t: &Track) -> Result<Option<String>> {
+    // let mut text: Option<String>;
+    let client = reqwest::Client::new();
     let query = format!("{}+{}+lyrics", t.name, t.artists).replace(" ", "+");
 
     // try google.com
@@ -104,33 +108,64 @@ async fn get_track_lyrics(t: &Track) -> Result<String> {
         "https://www.google.com/search?q={}&ie=utf-8&oe=utf-8",
         query
     );
-    // let url = "https://www.google.com/search?q=Someone+You+Loved+Lewis+Capaldi+lyrics&oe=utf-8";
-
-    let mut _text: Option<String>;
-
     // println!("{}", url);
 
-    let response = reqwest::get(&url).await?.text().await?;
+    let response = client.get(&url).send().await?.text().await?;
     // println!("{}", response);
 
     let fragment = Html::parse_fragment(&response);
     let elm = Selector::parse(r#"div[class="BNeawe tAd8D AP7Wnd"]"#).unwrap();
 
-    _text = match fragment.select(&elm).skip(3).next() {
+    let text = match fragment.select(&elm).last() {
         Some(elm) => match elm.first_child() {
             Some(node) => Some(node.value().as_text().unwrap().to_string()),
             _ => None,
         },
         _ => None,
     };
+    // println!("{:?}", text.clone());
 
-    // try another site
-    if _text.is_none() {
-        todo!()
+    // TODO: change it
+    // try greatlyrics.net
+    if text.is_none() {
+        // let url = format!(
+        //     "http://greatlyrics.net/search.html?q={}&ie=utf-8&oe=utf-8",
+        //     query
+        // );
+        // let response = client.get(&url).send().await?.text().await?;
+        // let fragment = Html::parse_fragment(&response);
+        // let elm = Selector::parse("a.txt-primary").unwrap();
+        // let url = fragment
+        //     .select(&elm)
+        //     .next()
+        //     .unwrap()
+        //     .value()
+        //     .attr("href")
+        //     .unwrap();
+
+        // let response = client.get(url).send().await?.text().await?;
+        // let fragment = Html::parse_fragment(&response);
+        // let elm = Selector::parse(r#"p[id="lyrics"]"#).unwrap();
+
+        // text = match fragment.select(&elm).next() {
+        //     Some(elm) => match elm.first_child() {
+        //         Some(node) => {
+        //             let s = node.value().as_text().unwrap().to_string();
+        //             if s.trim() != "" {
+        //                 Some(s.trim().to_string())
+        //             } else {
+        //                 None
+        //             }
+        //         }
+        //         _ => None,
+        //     },
+        //     _ => None,
+        // };
+
+        // println!("{:?}", text.clone());
     };
-    // println!("{}", _text.clone().unwrap());
 
-    Ok(_text.unwrap())
+    Ok(text)
 }
 
 async fn get_track(spotify: &Spotify) -> Result<Track> {
@@ -156,10 +191,18 @@ async fn init_spotify() -> Result<Spotify> {
     let client_secret = env::var("CLIENT_SECRET").unwrap_or_default();
     let redirect_uri = env::var("REDIRECT_URI").unwrap_or_default();
 
+    // path for cache token
+    let path = [".config", "spotifyrics"].iter().collect::<PathBuf>();
+    let mut home = PathBuf::from(env::var("HOME").unwrap_or_default());
+    home.push(path);
+    create_dir_all(&home).await?;
+    home.push("token_cache.json");
+
     let mut oauth = SpotifyOAuth::default()
         .client_id(&client_id)
         .client_secret(&client_secret)
         .redirect_uri(&redirect_uri)
+        .cache_path(home)
         .scope("user-read-currently-playing")
         .build();
     let token_info = get_token(&mut oauth).await.ok_or("auth failed")?;
